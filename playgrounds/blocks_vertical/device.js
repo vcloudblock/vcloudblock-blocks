359b2f6571dfb414191f3ed5cb9ff98015a7f170
Arthur <869705086@qq.com>
2021-03-01 10:49:39 +0000

    Build for 0b53134bcffd26efd284d4744305446e673a0620

--- playgrounds/blocks_vertical/device.js
+++ playgrounds/blocks_vertical/device.js
@@ -62,3 +62,98 @@ Blockly.Blocks['event_whenmicrobitbegin'] = {
     });
   }
 };
+
+Blockly.Blocks['event_whenmicrobitbuttonpressed'] = {
+  /**
+   * Block for when microbit button pressed.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "id": "event_whenmicrobitbuttonpressed",
+      "message0": Blockly.Msg.EVENT_WHENMICROBITBUTTONPRESSED,
+      "args0": [
+        {
+          "type": "field_dropdown",
+          "name": "KEY_OPTION",
+          "options": [
+            ['A', 'a'],
+            ['B', 'b'],
+            ['A+B', 'ab']
+          ]
+        }
+      ],
+      "nextStatement": null,
+      "category": Blockly.Categories.event,
+      "colour": Blockly.Colours.event.primary,
+      "colourSecondary": Blockly.Colours.event.secondary,
+      "colourTertiary": Blockly.Colours.event.tertiary
+    });
+  }
+};
+
+Blockly.Blocks['event_whenmicrobitpinbeingtouched'] = {
+  /**
+   * Block for when microbit pin was touched.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "id": "event_whenmicrobitpinbeingtouched",
+      "message0": Blockly.Msg.EVENT_WHENMICROBITPINBEINGTOUCHED,
+      "args0": [
+        {
+          "type": "field_dropdown",
+          "name": "PIN_OPTION",
+          "options": [
+            ['0', '0'],
+            ['1', '1'],
+            ['2', '2']
+          ]
+        }
+      ],
+      "nextStatement": null,
+      "category": Blockly.Categories.event,
+      "colour": Blockly.Colours.event.primary,
+      "colourSecondary": Blockly.Colours.event.secondary,
+      "colourTertiary": Blockly.Colours.event.tertiary
+    });
+  }
+};
+
+Blockly.Blocks['event_whenmicrobitgesture'] = {
+  /**
+   * Block for when microbit gesture is XXX.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "id": "event_whenmicrobitgesture",
+      "message0": Blockly.Msg.EVENT_WHENMICROBITGESTURE,
+      "args0": [
+        {
+          "type": "field_dropdown",
+          "name": "GESTURE_OPTION",
+          "options": [
+            [Blockly.Msg.EVENT_WHENMICROBITGESTURE_SHAKEN, 'shake'],
+            [Blockly.Msg.EVENT_WHENMICROBITGESTURE_TILTEDUPWARD, 'up'],
+            [Blockly.Msg.EVENT_WHENMICROBITGESTURE_TILTEDDOWNWARD, 'down'],
+            [Blockly.Msg.EVENT_WHENMICROBITGESTURE_TILTEDDLEFTWARD, 'left'],
+            [Blockly.Msg.EVENT_WHENMICROBITGESTURE_TILTEDDRIGHTWARD, 'right'],
+            [Blockly.Msg.EVENT_WHENMICROBITGESTURE_FACEUP, 'face up'],
+            [Blockly.Msg.EVENT_WHENMICROBITGESTURE_FACEDOWN, 'face down'],
+            [Blockly.Msg.EVENT_WHENMICROBITGESTURE_FREEFALL, 'freefall'],
+            ['3g', '3g'],
+            ['6g', '6g'],
+            ['8g', '8g']
+          ]
+        }
+      ],
+      "nextStatement": null,
+      "category": Blockly.Categories.event,
+      "colour": Blockly.Colours.event.primary,
+      "colourSecondary": Blockly.Colours.event.secondary,
+      "colourTertiary": Blockly.Colours.event.tertiary
+    });
+  }
+};
